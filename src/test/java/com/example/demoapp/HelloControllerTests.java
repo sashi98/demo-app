02c5f4bd77f8328d6dbca97979708fdf0ba0a8be
sashi98 <sashi98@gmail.com>
2022-11-14 16:28:12 +0530

    passing test

--- src/test/java/com/example/demoapp/HelloControllerTests.java
+++ src/test/java/com/example/demoapp/HelloControllerTests.java
@@ -28,6 +28,6 @@ public class HelloControllerTests {
         mvc.perform(MockMvcRequestBuilders.get("/")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-                .andExpect(MockMvcResultMatchers.content().string("Hello Sashi3 World"));
+                .andExpect(MockMvcResultMatchers.content().string("Hello batch 7 World"));
     }
 }
